b1f0cde59937a864c7dd57a8304f545d80df6e55
rianer <aerhan2001@gmail.com>
2026-01-28 00:34:45 +0200

    Add CORS policy;

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,23 @@ import express, { Request, Response } from "express";
 import { readTestDefinition } from "./services/tests.service";
 
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const corsOptions = {
+    origin: "http://localhost:5173", // WHITELIST the frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+    credentials: true, // Enable set cookie
+};
+
+// Enable CORS
+app.use(cors(corsOptions));
+
+// Optionally handle preflight requests
+app.options("/test/random", cors(corsOptions));
 
 app.get("/test/random", (req: Request, res: Response) => {
     const parsed = readTestDefinition("data-types");
